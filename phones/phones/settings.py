1cd6792a79fa81dd1b102bda5bcca7748937d12f
Amin Masoudi <aminmasoudi2003@gmail.com>
2023-08-16 01:47:45 +0330

    [CNf] final configurations

--- phones/phones/settings.py
+++ phones/phones/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6@$*yiym8q4n#qztm86qhjp+*r_7rr%vdgscw&m%-oc3ny6w30
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -80,26 +80,26 @@ WSGI_APPLICATION = 'phones.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'OPTIONS': {
-#             'read_default_file': f'{BASE_DIR}/mariadb.cnf',
-#         },
-#     }
-# }
-
-
-
-
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': f'{BASE_DIR}/mariadb.cnf',
+        },
     }
 }
+
+
+
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
