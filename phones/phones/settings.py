765ec7abaea4efb6a37fe3264a547d3d3b5bd913
Amin Masoudi <aminmasoudi2003@gmail.com>
2023-08-16 00:31:58 +0330

    [init] Logger

--- phones/phones/settings.py
+++ phones/phones/settings.py
@@ -142,3 +142,43 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers":False,
+
+    "formatters": {
+        "verbose": {
+            "format": "[{levelname}] {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} : {message}",
+            "style": "{"
+        }
+    },
+    
+    "handlers":{
+        "debug_logs":{
+            "level":"DEBUG",
+            "class": "logging.FileHandler",
+            "filename": BASE_DIR / "LOGS/DEBUG.log",
+            "formatter": "simple"
+        },
+        "warnings_log":{
+            "level": "WARNING",
+            "class": "logging.FileHandler",
+            "filename": BASE_DIR / "LOGS/WARNING.log",
+            "formatter": "verbose"
+        },
+
+    },
+    "loggers":{
+        "":{
+            "level": 'DEBUG',
+            "handlers":["warnings_log",'debug_logs'],
+        },
+
+    }
+}
